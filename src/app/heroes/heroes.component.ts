f07a5fc77efa65fb8b33af0a24a2a2927d2e98b1
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2021-10-25 17:42:38 +0700

    Final Project Angular Tour of Heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] =[];
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(x => {
+          console.log(x);
+          this.heroes = x;
+        });
+  }
+
+  onSelect(hero: Hero): void {
+    this.messageService.add(`You selected hero with id ${hero.id} and name ${hero.name}`);
+    console.log(hero);
+    this.selectedHero = hero;
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if(!name){ return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+}
